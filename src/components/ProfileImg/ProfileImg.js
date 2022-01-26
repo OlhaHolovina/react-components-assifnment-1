f7b19199a0e53800f3628d5823ca2d2fede4dc19
Ola <ol.holovina@gmail.com>
2022-01-26 08:46:22 -0500

    img and header styles fix

--- src/components/ProfileImg/ProfileImg.js
+++ src/components/ProfileImg/ProfileImg.js
@@ -1,9 +1,10 @@
 import profileImg from '../../img/olha_holovina_pict.jpg';
+import './style.css'
 
 function ProfileImg() {
   return (
     <aside>
-      <img src={profileImg} alt="Olha Holovina" />
+      <img src={profileImg} alt="Olha Holovina" className='profile-img' />
     </aside>
   );
 }
